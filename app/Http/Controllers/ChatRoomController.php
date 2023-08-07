fdc0b1d98246832ca02429e4e7f2022c8abb5ede
EslamBanna <solombana2000@gmail.com>
2023-08-07 17:47:32 +0300

    fix chatrooms design

--- app/Http/Controllers/ChatRoomController.php
+++ app/Http/Controllers/ChatRoomController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\ChatRoom;
 use App\Traits\GeneralTrait;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ChatRoomController extends Controller
 {
@@ -32,16 +33,18 @@ class ChatRoomController extends Controller
     {
         try {
             $chat_rooms = ChatRoom::where('user_id', auth()->user()->id)
-            ->orWhere('friend_id', auth()->user()->id)
-            ->with(['user' => function($q){
-                $q->select('id', 'name','image') ->where('id', '!=', auth()->user()->id);
-            }])
-            ->with(['friend' => function($q){
-                $q->select('id', 'name','image') ->where('id', '!=', auth()->user()->id);
-            }])
-            ->with('last_message:user_id,message,chat_room_id,created_at', 'last_message.user:id,name,image')
-            ->withCount('unread_messages')
-            ->get();
+                ->orWhere('friend_id', auth()->user()->id)
+                ->with(['user' => function ($q) {
+                    $q->select('id', 'name', 'image')->where('id', '!=', auth()->user()->id);
+                }])
+                ->with(['friend' => function ($q) {
+                    $q->select('id', 'name', 'image')->where('id', '!=', auth()->user()->id);
+                }])
+                ->with('last_message:user_id,message,chat_room_id,created_at', 'last_message.user:id,name,image')
+                ->withCount('unread_messages')
+                // ->orderBy('last_message.created_at', 'desc')
+                ->orderBy('updated_at', 'desc')
+                ->get();
             return $this->returnData('chat_rooms', $chat_rooms);
         } catch (\Exception $e) {
             return $this->returnError('E001', $e->getMessage());
@@ -51,14 +54,21 @@ class ChatRoomController extends Controller
     public function getChatMessages($friend_id)
     {
         try {
+            DB::beginTransaction();
             $chat_room = ChatRoom::where('user_id', auth()->user()->id)
                 ->where('friend_id', $friend_id)->first();
             if (!$chat_room) {
                 return $this->returnError('E001', 'you dont have a chat room with this user');
             }
             $messages = $chat_room->messages;
+            $messages->where('user_id', '!=', auth()->user()->id)->where('seen', false)->each(function ($message) {
+                $message->seen = true;
+                $message->save();
+            });
+            DB::commit();
             return $this->returnData('messages', $messages);
         } catch (\Exception $e) {
+            DB::rollBack();
             return $this->returnError('E001', $e->getMessage());
         }
     }
@@ -66,17 +76,27 @@ class ChatRoomController extends Controller
     public function sendMessage(Request $request, $friend_id)
     {
         try {
+            DB::beginTransaction();
             $chat_room = ChatRoom::where('user_id', auth()->user()->id)
                 ->where('friend_id', $friend_id)->first();
             if (!$chat_room) {
                 return $this->returnError('E001', 'you dont have a chat room with this user');
             }
+            if ($chat_room->user_id != auth()->user()->id  || $chat_room->friend_id != $friend_id) {
+                return $this->returnError('E001', 'you dont have a chat room with this user');
+            }
+            if (!$request->message) {
+                return $this->returnError('E001', 'message is required');
+            }
             $chat_room->messages()->create([
                 'user_id' => auth()->user()->id,
                 'message' => $request->message
             ]);
+            $chat_room->updated_at = now();
+            DB::commit();
             return $this->returnSuccessMessage('message sent successfully');
         } catch (\Exception $e) {
+            DB::rollBack();
             return $this->returnError('E001', $e->getMessage());
         }
     }
