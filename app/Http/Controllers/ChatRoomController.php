151e32964b8397b344b29b6b8eb07bba1380d5fc
EslamBanna <solombana2000@gmail.com>
2023-09-02 14:04:51 +0300

    add new chatroom

--- app/Http/Controllers/ChatRoomController.php
+++ app/Http/Controllers/ChatRoomController.php
@@ -16,16 +16,16 @@ class ChatRoomController extends Controller
     public function addChatRoom(Request $request)
     {
         try {
-            $check_if_chat_room_is_exist = ChatRoom::where('user_id', auth()->user()->id)
-                ->where('friend_id', $request->friend_id)->first();
+            $check_if_chat_room_is_exist = ChatRoom::where([['user_id', auth()->user()->id], ['friend_id', $request->friend_id]])
+                ->orWhere([['friend_id', auth()->user()->id], ['user_id', $request->friend_id]])->first();
             if ($check_if_chat_room_is_exist) {
-                return $this->returnError('E001', 'you have a chat room with this user');
+                return $this->returnData('chatroom_id', $check_if_chat_room_is_exist->id);
             }
-            ChatRoom::create([
+            $new_chat_room =  ChatRoom::create([
                 'user_id' => auth()->user()->id,
                 'friend_id' => $request->friend_id
             ]);
-            return $this->returnSuccessMessage('chat room added successfully');
+            return $this->returnData('chatroom_id', $new_chat_room->id);
         } catch (\Exception $e) {
             return $this->returnError('E001', $e->getMessage());
         }
@@ -57,9 +57,6 @@ class ChatRoomController extends Controller
     {
         try {
             DB::beginTransaction();
-            // $chat_room = ChatRoom::where([['friend_id', $friend_id], ['user_id', auth()->user()->id]])
-            // ->orWhere([['user_id', $friend_id], ['friend_id', auth()->user()->id]])
-            // ->first();
             $chat_room = ChatRoom::find($chat_room_id);
             if (!$chat_room) {
                 return $this->returnError('E001', 'you dont have a chat room with this user');
@@ -72,11 +69,6 @@ class ChatRoomController extends Controller
             $messages = Message::where('chat_room_id', $chat_room_id)
                 ->with('user:id,name,image')
                 ->get();
-            // $data = [
-            //     'user' => auth()->user(),
-            //     'friend' => $chat_room->friend,
-            //     'messages' => $messages
-            // ];
             DB::commit();
             return $this->returnData('messages', $messages);
         } catch (\Exception $e) {
@@ -89,17 +81,9 @@ class ChatRoomController extends Controller
     {
         try {
             DB::beginTransaction();
-            // $chat_room = ChatRoom::where([['friend_id', $friend_id], ['user_id', auth()->user()->id]])
-            //     ->orWhere([['user_id', $friend_id], ['friend_id', auth()->user()->id]])
-            //     ->first();
-
             $chat_room = ChatRoom::find($chat_room_id);
-
             if (!$chat_room) {
-                $chat_room = ChatRoom::create([
-                    'user_id' => auth()->user()->id,
-                    'friend_id' => $request->friend_id
-                ]);
+                return $this->returnError('E001', 'you dont have a chat room with this user');
             }
             if (!$request->message) {
                 return $this->returnError('E001', 'message is required');
